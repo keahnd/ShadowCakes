f80ab1207ca8354682f5f787c12e48487e3fa299
Keahn Divecha <keahnd@gmail.com>
2021-06-06 02:58:00 -0700

    Changed seed file, removed test products

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,35 +12,3 @@ User.create!(email: ENV["shadowsCakesEmail"], password: ENV["shadowsCakesAdminPa
 Category.create!(heading: "Brownie", body: "Its yum", display: true, id: 1)
 Category.create!(heading: "Cakes", body: "The Shadow kind", display: true, id:2)
 Category.create!(heading: "Cupcakes", body: "Little Ones", display: true, id:3)
-
-5.times do |i|
-	brownie = Product.new(
-		name: "brownie",
-		description: "chocolate and baked",
-		price: 14.99,
-		featured: true,
-		contains_peanuts: false,
-		category: Category.find(1)
-	)
-
-	cakes = Product.new(
-		name: "cakes",
-		description: "chocolate and baked",
-		price: 20.99,
-		featured: true,
-		contains_peanuts: false,
-		category: Category.find(2)
-	)
-
-	cupcakes = Product.new(
-		name: "cupcake",
-		description: "chocolate and baked",
-		price: 8.99,
-		featured: true,
-		contains_peanuts: false,
-		category: Category.find(3)
-	)
-	brownie.save
-	cakes.save
-	cupcakes.save
-end
