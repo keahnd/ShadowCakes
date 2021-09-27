5838a822dd7a7674d086c4e8dbd02213030505e9
Keahn Divecha <keahnd@gmail.com>
2021-09-26 20:22:05 -0400

    email

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: ENV["shadowsCakesEmail"], password: ENV["shadowsCakesAdminPass"],
-			 password_confirmation: ENV["shadowsCakesAdminPass"], admin: true)
+User.create!(email: "shadowscakes.bakery@gmail.com", password: "cookie$_pass",
+			 password_confirmation: "cookie$_pass", admin: true)
+
+Category.create!(heading: "Brownie", display: true, id: 1)
+Category.create!(heading: "Cakes", display: true, id:2)
+Category.create!(heading: "Cupcakes", display: true, id:3)
